316051a390b4bb310456606b2b03bcb74d42e67c
Mark Corpuz <social@markcorpuz.com>
2017-08-09 07:35:37 -0700

    Credit Footer Section - Applied updates specific to SPK based on default setup

--- functions.php
+++ functions.php
@@ -153,12 +153,13 @@ function setupbasic_comments_gravatar( $args ) {
 
 }
 
-//* Customize the entire footer | functions-sitefooter
+//* Add the credits section on the site footer
 remove_action( 'genesis_footer', 'genesis_do_footer' );
-add_action( 'genesis_footer', 'spt_sitefooter' );
-function spt_sitefooter() {
+add_action( 'genesis_footer', 'swp_sitefooter_credit' );
+function swp_sitefooter_credit() {
 	?>
 	<div class="siteby"><a href="http://smarterwebpackages.com/">SmarterWebPackages.com</a></div>
-	<div class="copyright">Copyright © <?php echo date("Y"); ?> Smarter Better · All Rights Reserved | <a href="http://smarterwebpackages.com/">Website</a></div>
+	<div class="copyright">Copyright © <?php echo date("Y"); ?> Smarter Better · All Rights Reserved<br>
+	Site Design by <a href="http://smarterwebpackages.com/">SmarterWebPackages.com</a></div>
 	<?php
 }
